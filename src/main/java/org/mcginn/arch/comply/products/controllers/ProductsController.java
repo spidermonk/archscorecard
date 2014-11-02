36937d41d1913f13f25c3ebf3af2be4c426d3608
spidermonk <jmcginn13@gmail.com>
2014-11-02 15:42:06 -0500

    adding logging

--- src/main/java/org/mcginn/arch/comply/products/controllers/ProductsController.java
+++ src/main/java/org/mcginn/arch/comply/products/controllers/ProductsController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.mcginn.arch.comply.products.Product;
 import org.mcginn.arch.comply.products.db.ProductDb;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductsController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(ProductsController.class);
+	
 	private ProductDb productDb;
 	
 	@RequestMapping(method=RequestMethod.GET, produces={"application/json"})
